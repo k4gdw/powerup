9d927478281aefc91e1bd95d88f04f8cf440ce8f
Bryan Johns <bjohns@greendragonweb.com>
2013-02-11 10:05:01 -0600

    filled out the build.msbuild and added build.bat

---
+++ Build/build.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+REM Build.bat
+REM Created by:  Bryan Johns
+REM Copyright:	 K4GDW Software 2012
+
+IF NOT "%1"=="" (
+	IF "%1"=="/build" (
+		IF NOT "%2"=="" (
+			SET build=%2
+			msbuild.exe build.msbuild /t:Publish /p:BuildNo=%build%;LocalBuild=True /m
+		) ELSE (
+			ECHO You must supply a build number.
+		)
+	)
+) ELSE (
+	msbuild build.xml /t:Publish /p:LocalBuild=True /m
+)
